217a16f6af84206bd32f247d603e005bba7203c4
Tristan Viney <yenivyeniv@gmail.com>
2019-07-20 22:57:17 +0200

    still working on adding content

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,3 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!
-
-  def home
-  end
-
-  def resume
-  end
-
-  def about
-  end
-
-  def contact
-  end
 end
