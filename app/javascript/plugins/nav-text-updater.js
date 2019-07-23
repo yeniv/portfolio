fdfc1f39dc7de29dc0ad1169d5faa10f080e68ce
Tristan Viney <yenivyeniv@gmail.com>
2019-07-23 23:07:04 +0200

    saving work here

--- app/javascript/plugins/nav-text-updater.js
+++ app/javascript/plugins/nav-text-updater.js
@@ -1,3 +1,17 @@
+changeText = (scrollPosition, top, bottom) => {
+  return (scrollPosition > top) && (scrollPosition < bottom)
+}
+
+fadeInText = (newText) => {
+  if (text.innerHTML != newText) {
+      text.innerHTML = newText
+      text.classList.remove('nav-text-fadeout')
+      text.classList.remove('nav-text-fadein')
+      text.offsetWidth = text.offsetWidth;
+      text.classList.add('nav-text-fadein')
+    }
+}
+
 const text = document.querySelector('.nav-text-update')
 let navHeight = document.querySelector('.nav-container').getBoundingClientRect().height
 
@@ -27,86 +41,17 @@ window.addEventListener(
 )
 
 // forrest case study
+// const forrestFinalDivTop = document.querySelector('#forrest-final').getBoundingClientRect().top - navHeight
+// const forrestFinalDivBottom = document.querySelector('#forrest-final').getBoundingClientRect().bottom - navHeight
 
-
-changeText = (scrollPosition, top, bottom) => {
-  return (scrollPosition > top) && (scrollPosition < bottom)
-}
-
-fadeInText = (newText) => {
-  if (text.innerHTML != newText) {
-      text.innerHTML = newText
-      text.classList.remove('nav-text-fadeout')
-      text.classList.remove('nav-text-fadein')
-      text.offsetWidth = text.offsetWidth;
-      text.classList.add('nav-text-fadein')
-    }
-}
-
-
-// is working
-// updateText = (newText) => {
-//   if (text.innerHTML != newText) {
-//       text.innerHTML = newText
-//       text.classList.remove('nav-text-fadein')
-//       text.offsetWidth = text.offsetWidth;
-//       text.classList.add('nav-text-fadein')
-//   }
-// }
-
-//fadein words... not fade out
-// fadeOutText = () => {
-//       text.classList.remove('nav-text-fadeout')
-//       text.offsetWidth = text.offsetWidth;
-//       text.classList.add('nav-text-fadeout')
-// }
-
-// fadeInText = (newText) => {
-//       text.innerHTML = newText
-//       text.classList.remove('nav-text-fadein')
-//       text.offsetWidth = text.offsetWidth;
-//       text.classList.add('nav-text-fadein')
-// }
-
-// textChange = (newText) => {
-//   if (text.innerHTML != newText) {
-//     // fadeOutText()
-//     fadeInText(newText)
-//     // setTimeout(() => {fadeInText(newText)}, 500)
-//   }
-// }
-
-
-// textChange = (newText) => {
-//   if (text.innerHTML != newText) {
-//     // if (text.classList.contains('nav-no-fadeout')) {
-//       text.innerHTML = newText
-//       text.classList.remove('nav-text-fadeout')
-//       text.classList.remove('nav-text-fadein')
-//       text.offsetWidth = text.offsetWidth;
-//       text.classList.add('nav-text-fadein')
-//     // } else {
-//     //   text.classList.add('nav-text-fadeout')
-//     //   setTimeout(() => {
-//     //     text.innerHTML = newText
-//     //     text.classList.remove('nav-text-fadein')
-//     //     text.classList.remove('nav-text-fadeout')
-//     //     text.offsetWidth = text.offsetWidth;
-//     //     text.classList.add('nav-text-fadein')
-//     //   }, 600)
-//     // }
-//   }
-// }
-
-// fadeOutText = (newText) => {
-//   if (text.innerHTML != newText) {
-//       text.classList.remove('nav-text-fadeout')
-//       text.offsetWidth = text.offsetWidth;
-//       text.classList.add('nav-text-fadeout')
-//       // setTimeout(() => {
-//         text.innerHTML = newText
-//       // }, 500)
+// window.addEventListener(
+// 'scroll', () => {
+//     let scrollPosition = window.scrollY
+//     if (changeText(scrollPosition, forrestFinalDivTop, forrestFinalDivBottom)) {
+//       fadeInText('IS CHUFFED YOU READ EVERYTHING')
+//     } else {
+//       fadeInText('')
 //     }
-// }
-
+//   }
+// )
 
