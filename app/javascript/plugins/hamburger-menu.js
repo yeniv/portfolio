5181313db6389dc715058712b09f3fa0579fdc4c
Tristan Viney <tristanviney@gmail.com>
2019-07-26 10:05:59 +0200

    Revert "Phone number"

--- app/javascript/plugins/hamburger-menu.js
+++ app/javascript/plugins/hamburger-menu.js
@@ -1,12 +1,12 @@
+console.log('hello from hamburger menu')
+
 const menuIcon = document.querySelector('.nav-bar-menu')
 const popup = document.querySelector('.nav-bar-menu-popup')
-const closeIcon = document.querySelector('.nav-bar-close')
 const link = document.querySelectorAll('.nav-bar-link')
 
 menuIcon.addEventListener('click', () => {
-  popup.classList.toggle('hidden')
-})
-
-closeIcon.addEventListener('click', () => {
+  menuIcon.classList.toggle('nav-bar-menu-open')
+  menuIcon.classList.toggle('fa-bars')
+  menuIcon.classList.toggle('fa-times')
   popup.classList.toggle('hidden')
 })
