82f15d662408373415510ac145feb0467029d32d
Tristan Viney <yenivyeniv@gmail.com>
2019-07-26 11:52:51 +0200

    deleted unnecesary js code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,3 @@ import "bootstrap";
 import "../plugins/hamburger-menu.js";
 import "../plugins/arrow_scroller.js";
 import "../plugins/nav-text-updater.js";
-
-
-if (location.pathname == '/') {
-    // import "../plugins/arrow_scroller.js";
-    // import "../plugins/nav-text-updater.js";
-    // import "../plugins/hamburger-menu.js";
-  } else {
-    // import "../plugins/hamburger-menu.js";
-  }
