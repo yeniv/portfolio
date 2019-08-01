c6de2427307249de1fc41c2685ef9907f029ebef
Tristan Viney <yenivyeniv@gmail.com>
2019-08-01 22:40:23 +0200

    hiive image file type upated

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,4 @@
 import "bootstrap";
 import "../plugins/hamburger-menu.js";
-// import "../plugins/scroll-reveal.js";
 import "../plugins/arrow_scroller.js";
 import "../plugins/nav-text-updater.js";
